ae3272aea8856d55117e4cc5d31db80718b97caa
Tejas Rajagopal <73415129+tejasr112002@users.noreply.github.com>
2021-11-08 01:49:46 +0800

    Fixed running decimal

--- src/cz2002/invoice/InvoiceMGR.java
+++ src/cz2002/invoice/InvoiceMGR.java
@@ -3,7 +3,7 @@ package invoice;
 import java.time.format.DateTimeFormatter;  
 import java.util.ArrayList;
 import java.util.List;
-
+import java.lang.Math;
 import cz2002.Customer;
 
 //import com.sun.tools.javac.util.List;
@@ -24,7 +24,7 @@ public class InvoiceMGR {
 	 * @param GST
 	 */
     public static final double DISCOUNT_RATE = 0.10; //Assuming that members get a 10% discount
-    public static final double GST_RATE = 0.07; 
+    public static final double GST_RATE = 0.07;
     public static final double SERVICE_CHARGE = 0.10;
 	/*public Invoice createInvoice(Staff staff, ArrayList<OrderItem> orderItems, int tableno, double servicechrg, double GST) {
 		// TODO - implement InvoiceMGR.createInvoice
@@ -49,30 +49,32 @@ public class InvoiceMGR {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");  
         LocalDateTime now = LocalDateTime.now();  
         String timestamp = now.format(dtf);
-        System.out.println("--------Invoice--------");
-        System.out.println("Table No: "+order.getTableno());
-        System.out.println("Time: "+timestamp);
+        System.out.println("------------Invoice------------");
+        System.out.println("Staff: "+order.getStaff().getName()+"\tDate: "+timestamp.substring(0,10));
+        System.out.println("Table: "+order.getTableno()+"\tTime: "+timestamp.substring(11));
+        System.out.println("-------------------------------");
         System.out.println("Order Items:");
-        System.out.println("Item Name\tItem Price");
+        System.out.println("Item Name\t\tItem Price");
         double total = 0.0;
         for(int j = 0;j<order.getOrderItems().size();j++)
         {
             System.out.println(order.getOrderItems().get(j).getName()+"\t"+order.getOrderItems().get(j).getPrice());
             total+= order.getOrderItems().get(j).getPrice();  
         }
-        System.out.println("Subtotal: = "+total+"SGD");
+        System.out.println("\tSubtotal: = "+total+"SGD");
         if(checkMember(order.getCustomer())){
             total = (1-DISCOUNT_RATE)*total; // Discount rate is set at 10% for members
-            //System.out.println("Subtotal after "+(DISCOUNT_RATE*100)"% discount = "+total);
-            System.out.println("Discount = "+(DISCOUNT_RATE*100)+"%");
+            System.out.println("\tDiscount = "+(DISCOUNT_RATE*100)+"%");
         }
-        System.out.println("GST = "+(GST_RATE*100)+"%");
-        System.out.println("Service Charge = "+(SERVICE_CHARGE*100)+"%");
+        System.out.printf("\tGST: %.1f", GST_RATE*100);
+        System.out.print("%\n");
+        System.out.printf("\tService Charge: %.1f", SERVICE_CHARGE*100);
+        System.out.print("%\n");
         double servicechrg = SERVICE_CHARGE*total;
         total+=servicechrg;
         double GST = GST_RATE*total;
         total+=GST;
-        System.out.println("TOTAL = "+total+"SGD");
+        System.out.println("\tTOTAL: "+total+"SGD");
         System.out.println("Thank you for dining with us!");
         order.setPaid(true);
         //Table.setOccupied(false, order.getTableno());
@@ -91,6 +93,7 @@ public class InvoiceMGR {
 	 * @param invoices
 	 */
 	public static void printSalesRevenueReport(String period, ArrayList<Invoice> invoices) {
+        System.out.println("Revenue Report:");
         if(period.equals("day")){
             for(int i = 0;i<invoices.size();i++){
                 double revenue = 0;
@@ -101,7 +104,7 @@ public class InvoiceMGR {
                     j++;
                 }
                 i=j;
-                System.out.println("Revenue for "+timestamp+" = "+revenue);
+                System.out.println(timestamp+"\t"+revenue);
             }
         }
         else if(period.equals("month")){
@@ -114,7 +117,7 @@ public class InvoiceMGR {
                     j++;
                 }
                 i=j;
-                System.out.println("Revenue for "+timestamp+" = "+revenue);
+                System.out.println(timestamp+"\t"+revenue);
             }
         }
 	}
