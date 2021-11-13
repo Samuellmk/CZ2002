48dbed0ebbfc27f1d6df5ad6040adcde5dbcebe3
Samuel L. <samuel_leong@outlook.com>
2021-11-13 14:30:57 +0800

    Changes ReservationUI checkTableAvailUI

--- src/cz2002/reservation/ReservationUI.java
+++ src/cz2002/reservation/ReservationUI.java
@@ -240,7 +240,7 @@ public class ReservationUI {
 					if(ltDate.isAfter(LocalDate.now().minusDays(1)))
 						validDate =  true;
 					else {
-						System.out.println("Invalid date, try again...");
+						System.out.println("Invalid date");
 						validDate = false;
 					}
 				}catch(Exception e) {
